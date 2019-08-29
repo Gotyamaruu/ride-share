38d00bc62c59fee61351d002582e10f00e6d8b53
mizuno <inc.sinka@gmail.com>
2019-08-29 18:39:06 +0900

    capistrano-fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.11.0"
 
 set :application, "ride-share"
-set :repo_url, "git@github.com/Gotyamaruu/ride-share"
+set :repo_url, "git@github.com:Gotyamaruu/ride-share.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
